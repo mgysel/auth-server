d1341a6a5d9cf301d9e5f9b3a6300b70390e0516
Michael Gysel <mikegysel@gmail.com>
2020-05-17 20:26:49 +1000

    server function

--- app.py
+++ app.py
@@ -2,6 +2,52 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "HELLO WORLD"
+USER_DATA = []
+
+def defaultHandler(error):
+    '''
+    Handles flask errors
+    '''
+    response = error.get_response()
+    print('response', error, error.get_response())
+    response.data = dumps({
+        "code": error.code,
+        "name": "System Error",
+        "message": error.get_description(),
+    })
+    response.content_type = 'application/json'
+    return response
+app.register_error_handler(Exception, defaultHandler)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    user_info = request.get_json()
+
+    user = {
+        'email': user_info['email'],
+        'password': user_info['password'] 
+    }
+
+    USER_DATA.append(user)
+
+    # WHY IS THIS DONE? 
+    # Must be to update the state, unnecessary if do not need
+    return dumps(user)
+
+@app.route('/login', methods=['POST'])
+def login():
+    user_info = request.get_json()
+
+    user = {
+        'email': user_info['email'],
+        'password': user_info['password']
+    }
+
+    if (user in USER_DATA):
+        return dumps(user)
+    else:
+        raise InputError('Email or Password is Invalid')
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "Hello World!!!"
