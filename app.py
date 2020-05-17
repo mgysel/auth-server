a5006a2940409719dc74fb9b51d0cf04fd724648
Michael Gysel <mikegysel@gmail.com>
2020-05-17 21:41:28 +1000

    adsf

--- app.py
+++ app.py
@@ -4,60 +4,6 @@ from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
-USER_DATA = []
-
-class AccessError(HTTPException):
-    code = 400
-    message = 'No message specified'
-
-class InputError(HTTPException):
-    code = 400
-    message = 'No message specified'
-
-def defaultHandler(error):
-    '''
-    Handles flask errors
-    '''
-    response = error.get_response()
-    print('response', error, error.get_response())
-    response.data = dumps({
-        "code": error.code,
-        "name": "System Error",
-        "message": error.get_description(),
-    })
-    response.content_type = 'application/json'
-    return response
-app.register_error_handler(Exception, defaultHandler)
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    user_info = request.get_json()
-
-    user = {
-        'email': user_info['email'],
-        'password': user_info['password'] 
-    }
-
-    USER_DATA.append(user)
-
-    # WHY IS THIS DONE? 
-    # Must be to update the state, unnecessary if do not need
-    return dumps(user)
-
-@app.route('/login', methods=['POST'])
-def login():
-    user_info = request.get_json()
-
-    user = {
-        'email': user_info['email'],
-        'password': user_info['password']
-    }
-
-    if (user in USER_DATA):
-        return dumps(user)
-    else:
-        raise InputError('Email or Password is Invalid')
-
 @app.route('/', methods=['GET'])
 def test():
     return "Hello World!!!"
