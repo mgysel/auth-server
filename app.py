6278395efcedcfde67cdae10223967e395b44e2e
Michael Gysel <mikegysel@gmail.com>
2020-05-17 21:43:48 +1000

    adsa

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def test():
     return "Hello World!!!"
 
-#app.run(port='8089', debug=True)
+#app.run(port='8090', debug=True)
